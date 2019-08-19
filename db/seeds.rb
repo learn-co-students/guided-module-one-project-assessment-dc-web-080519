f2e5e53ee50123c19096284a587a6860bd61bc97
Pete Hanner <petehanner@gmail.com>
2019-08-19 12:15:52 -0400

    interests created

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+### INTERESTS ###
+Interest.find_or_create_by(name: "Food")
+Interest.find_or_create_by(name: "Board Games")
+Interest.find_or_create_by(name: "Photography")
+Interest.find_or_create_by(name: "Music")
+Interest.find_or_create_by(name: "Drinks")
+Interest.find_or_create_by(name: "Technology")
+Interest.find_or_create_by(name: "Fitness")
+Interest.find_or_create_by(name: "Nature")
+
+interests = Interest.all
+
+### EVENTS ###
+
+
+### USERS ###
+
+
