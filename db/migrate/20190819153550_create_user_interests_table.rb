ffb6fe0c1841906bd58a6fbd50093cbbb919d347
icydwei <robpaik@gmail.com>
2019-08-19 12:14:28 -0400

    fixed user_interests table

--- db/migrate/20190819153550_create_user_interests_table.rb
+++ db/migrate/20190819153550_create_user_interests_table.rb
@@ -1,6 +1,6 @@
 class CreateUserInterestsTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :userinterests do |t|
+    create_table :user_interests do |t|
       t.integer :user_id
       t.integer :interest_id
     end
