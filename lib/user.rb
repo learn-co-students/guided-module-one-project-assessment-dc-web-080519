ec07956cb64d7307915e28516b479c61df8e89a6
Pete Hanner <petehanner@gmail.com>
2019-08-20 17:39:51 -0400

    tweaking interface flow & User class

--- lib/user.rb
+++ lib/user.rb
@@ -4,40 +4,9 @@ class User < ActiveRecord::Base
   has_many :rsvps
   has_many :events, through: :rsvps
 
-  # def display_profile
-  #   puts "username: #{self.user_name}"
-  #   puts "Name: #{self.name}"
-  #   puts "Location: #{self.location}"
-  #   puts "Interests:"
-  #   self.display_interests
-  # end
-
-  # def input_to_index
-  #   input = gets.chomp
-  #   index = input.to_i - 1
-  # end
-
-  # def list_array(arr)
-  #   arr.each_with_index do |item, index|
-  #     puts "#{index + 1}. #{item.name}"
-  #   end
-  # end
-
   def add_interest(interest)
-    # # save array of possible new interests to variable
-    # possible_interests = Interest.all - self.interests
-    # # display list of possible interests
-    # puts "Please select an interest to add to your profile:"
-    # self.list_array(possible_interests)
-    # # user selects index of interest to add & is converted to array index
-    # index = self.input_to_index
-    # # User instance updated in Ruby
     self.interests << interest
   end
-
-  # def display_interests
-  #   self.list_array(self.interests)
-  # end
 
   def remove_interest
     # prompt user
@@ -55,16 +24,17 @@ class User < ActiveRecord::Base
 
   def find_events
     # compare user interests against events' interests
-    matching_events = Event.all.find_all do |event|
+    interested_events = Event.all.find_all do |event|
       # return any Events with overlapping Interests
       !(self.interests & event.interests).empty?
     end
     # print a list of matching events for the user
-    self.list_array(matching_events)
+    # TODO: further filter list of interested events by user location
+    self.list_array(interested_events)
     puts ""
     puts "Please select an event to see more details on."
     index = self.input_to_index
-    matching_events[index].display_details
+    interested_events[index].display_details
   end
 
   def rsvp_to(event)
