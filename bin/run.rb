7eb2a50a4f5599517251f96a36c787bce181f829
Pete Hanner <petehanner@gmail.com>
2019-08-22 17:00:44 -0400

    Username validation & method cleanup

--- bin/run.rb
+++ bin/run.rb
@@ -1,38 +1,3 @@
 require_relative '../config/environment'
-#
-# User.destroy_all
-# UserInterest.destroy_all
-# Event.destroy_all
-# InterestEvent.destroy_all
-# Rsvp.destroy_all
-
-# user1 = User.create(user_name: "user1", name: "Rob", location: "Washington D.C.")
-# user1.interests = [Interest.find_by(name: "Photography"), Interest.find_by(name: "Nature")]
-#
-# event1 = Event.find_or_create_by(
-#   name: "Smash Party",
-#   description: "Smash Bros rules",
-#   location: "Washington D.C.",
-#   event_datetime: "2019-08-29 [08:30:00] "
-# )
-# event1.assign_interests(["Board Games", "Nature"])
-#
-# event2 = Event.find_or_create_by(
-#   name: "Street Festival",
-#   location: "New York City"
-# )
-# event2.assign_interests(["Food", "Photography"])
-#
-# event3 = Event.find_or_create_by(
-#     name: "Drink N Code",
-#     location: "Chicago"
-# )
-# event3.assign_interests(["Technology", "Drinks"])
-#
-# event4 = Event.find_or_create_by(
-#     name: "Photos of Birds",
-#     location: "Washington D.C.")
-# event4.assign_interests(["Photography", "Nature"])
-
 
 CommandLineInterface.runner
--- lib/cli.rb
+++ lib/cli.rb
@@ -60,13 +60,17 @@ class CommandLineInterface
   def create_new_user
     self.clear
     input = PROMPT.ask("Please enter a desired user name: ")
-    # check for special conditions in input or if username exists
-    # otherwise proceed to #build_profile with given username
     if input.downcase == 'back'
       self.welcome
     elsif input.downcase == 'exit'
       self.logout
+    elsif !(input =~ /\A[a-z\d][a-z\d_]*[a-z\d]\z/i)
+      # validate username
+      puts "Usernames can only have characters, numbers, or underscores."
+      sleep 0.7
+      self.create_new_user
     elsif User.find_by(user_name: input)
+      # check if username is taken
       puts "That user name already exists."
       sleep 0.7
       self.create_new_user
@@ -88,245 +92,255 @@ class CommandLineInterface
   # assign name attribute with input
   def set_name
     self.clear
-    self.user.name = PROMPT.ask("Please enter your full name: ")
+    input = PROMPT.ask("Please enter your full name: ")
+    # validate user name only has single spaces and valid characters
+    if (input == input.split.join(" ") && !(input =~ /[^a-z\s'.-]/i))
+      self.user.name = input
+    else
+      puts "Please enter a valid name"
+      sleep 1
+      self.set_name
+    end
   end
 
   # provide list of valid locations to set location attribute
   def choose_location
     self.clear
     # if user has location, put it in an array so only new locations are shown
-    current_location = []
-    current_location << self.user.location
-    locations =
-    [
+    locations = [
       "Washington D.C.",
       "New York City",
       "Chicago",
       "Los Angeles"
-      ]-current_location # subtract arrays to remove user location
+    ]
 
-      # tack special options to end of menu
-      locations << ["[] BACK", "[] LOGOUT"]
+    # subtract arrays to remove user location
+    current_location = []
+    current_location << self.user.location
+    locations = (locations - current_location)
 
-      input = PROMPT.select("Please choose your location", locations)
-      # check for special options, otherwise set location
-      if input == "[] BACK"
-        self.display_user_profile
-      elsif input == "[] LOGOUT"
-        self.logout
-      else
-        self.user.location = input
-      end
+    # tack special options to end of menu
+    locations << ["[] BACK", "[] LOGOUT"]
+
+    input = PROMPT.select("Please choose your location", locations)
+    # check for special options, otherwise set location
+    if input == "[] BACK"
+      self.display_user_profile
+    elsif input == "[] LOGOUT"
+      self.logout
+    else
+      self.user.location = input
     end
+  end
 
-    # let user add interests
-    def choose_interests
-      self.clear
-      # list all available interests the user hasn't added to their profile
-      possible_interests = (Interest.all - self.user.interests).map do |interest|
-        interest.name
-      end
-      if possible_interests == []
-        # check if user has already selected all interests
-        self.clear
-        puts "There are no additional interests to select. Returning you to your profile."
-        sleep 1
-        self.display_user_profile
-      else
-        # let user select multiple interests to add to their profile
-        selected_interests = PROMPT.multi_select("Please select one or more interests to add to your profile\n Press 'SPACE' to add and 'ENTER' to finish\n", possible_interests, echo:false, per_page:20)
-        # associate all chosen interests to user
-        selected_interests.each do |interest_name|
-          self.user.add_interest(Interest.find_by(name: interest_name))
-        end
-        self.user.save
-        self.display_user_profile
-      end
+  # let user add interests
+  def choose_interests
+    self.clear
+    # list all available interests the user hasn't added to their profile
+    possible_interests = (Interest.all - self.user.interests).map do |interest|
+      interest.name
     end
-
-    ########## RETURNING USER ###############################
-
-    # handle returning users
-    def login_page
+    if possible_interests == []
+      # check if user has already selected all interests
       self.clear
-      input = PROMPT.ask("Welcome back, please enter your username: ")
-      if input.downcase == "back"
-        self.welcome
-      elsif input.downcase == 'exit'
-        self.logout
-      elsif User.find_by(user_name: input)
-        # valid input
-        @user = User.find_by(user_name: input)
-        self.display_user_profile
-      else
-        # if username not found, let them try again or go to new user creation
-        PROMPT.select("That username does not exist") do |menu|
-          menu.choice "Try Again", -> {self.login_page}
-          menu.choice "Create New User", -> {self.create_new_user}
-        end
+      puts "There are no additional interests to select. Returning you to your profile."
+      sleep 1
+      self.display_user_profile
+    else
+      # let user select multiple interests to add to their profile
+      selected_interests = PROMPT.multi_select("Please select one or more interests to add to your profile\n Press 'SPACE' to add and 'ENTER' to finish\n", possible_interests, echo:false, per_page:20)
+      # associate all chosen interests to user
+      selected_interests.each do |interest_name|
+        self.user.add_interest(Interest.find_by(name: interest_name))
       end
-    end
-
-    ########## MAIN PROFILE PAGE ###############################
-
-    # landing page for user profile
-    def display_user_profile
-      self.clear
-      # profile information
-      puts "username: #{self.user.user_name}"
-      puts
-      puts "Name: #{self.user.name}"
-      puts "Location: #{self.user.location}"
-      puts "Interests:"
-      self.list_array(self.user.interests)
-      puts
-      # menu options
-      PROMPT.select ("") do |menu|
-        menu.choice "View Events", -> {self.find_events}
-        menu.choice "View your RSVPs", -> {self.show_rsvps}
-        menu.choice "Edit Profile\n", -> {self.profile_edit}
-        menu.choice "[] LOGOUT", -> {self.logout}
-      end
-    end
-
-    ########## EDITING PROFILE ###############################
-
-    # let user choose various profile information to edit
-    def profile_edit
-      self.clear
-      PROMPT.select("Choose your profile information to edit:") do |menu|
-        menu.choice "Name", -> {self.set_name}
-        menu.choice "Location", -> {self.choose_location}
-        menu.choice "Interests\n", -> {self.add_or_remove_interests}
-        menu.choice "[] BACK", -> {self.display_user_profile}
-        menu.choice "[] LOGOUT", -> {self.logout}
-      end
-      # destination methods would terminate program, so:
-      # save user and reload page once any edits are made
       self.user.save
-      self.profile_edit
+      self.display_user_profile
     end
+  end
 
-    # forked choice for user
-    def add_or_remove_interests
-      self.clear
-      PROMPT.select("") do |menu|
-        menu.choice "Add new interests", -> {self.choose_interests}
-        menu.choice "Remove current interests\n", -> {self.remove_interests}
-        menu.choice "[] BACK", -> {self.profile_edit}
-        menu.choice "[] LOGOUT", -> {self.logout}
-      end
-    end
+  ########## RETURNING USER ###############################
 
-    # user can remove interests they've added to profile
-    def remove_interests
-      self.clear
-      # make sure interests are there to be removed
-      if self.user.interests == []
-        puts "Please add some interests before you try to remove any."
-        sleep 1.5
-        self.add_or_remove_interests
-      else
-        current_interests = self.user.interests.map do |interest|
-          interest.name
-        end
-        # let user select multiple interests to remove
-        selected_interests = PROMPT.multi_select("Please select one or more interests you wish to remove from your profile\n Press 'SPACE' to select and 'ENTER' to finish\n", current_interests, echo:false, per_page:20)
-        # disassociate user from events
-        selected_interests.each do |interest_name|
-          self.user.remove_interest(Interest.find_by(name: interest_name))
-        end
-        # save user and return to option screen
-        self.user.save
-        self.add_or_remove_interests
-      end
-    end
-
-    ########## FIND EVENTS ################
-
-    # show user a list of events in their area matching their interests
-    def find_events
-      self.clear
-      matched_events = self.user.matching_events
-      if self.user.location == nil
-        # make sure location is set
-        puts "Please set your location to see events in your area."
-        sleep 1.5
-        self.display_user_profile
-      elsif self.user.interests == []
-        # make sure interests are set
-        puts "Please choose some interests so we can show you matching events."
-        sleep 1.5
-        self.display_user_profile
-      elsif matched_events == []
-        # just in case there are no matching events
-        puts "No events found in your location matching your interests."
-        sleep 1.5
-        self.display_user_profile
-      else
-        # populate list of matching events
-        # define code to run for each event if selected
-        # tack navigation options onto bottom of list
-        choices = {}
-        matched_events.each do |event|
-          choices[event.name] = "self.display_event_details('#{event.name}', 'self.find_events')"
-        end
-        choices["[] BACK"] = 'self.display_user_profile'
-        choices["[] LOGOUT"] = 'self.logout'
-        # user selection returns as method call in string format; execute that call
-        eval(PROMPT.select("Choose an event to see more details:", choices, per_page:45))
-      end
-    end
-
-    # a more detailed view for a given event
-    def display_event_details(eventname, backpage)
-      self.clear
-      event = Event.find_by(name: eventname)
-      puts "Event Name: #{event.name}"
-      puts "Event Description: #{event.description}"
-      puts "Location: #{event.location}"
-      puts "Date/Time: #{event.event_datetime}"
-      puts "Attendees:"
-      self.list_array(event.users)
-      puts
-
-      # let user add or remove RSVP for event
-      PROMPT.select("") do |menu|
-        if self.user.events.include?(event)
-          menu.choice "Remove your RSVP for this event\n", -> {self.user.remove_event(event)}
-        else
-          menu.choice "RSVP for this event\n", -> {self.user.rsvp_to(event)}
-        end
-        menu.choice "[] BACK", -> {eval(backpage)}
-        menu.choice "[] LOGOUT", -> {self.logout}
-      end
-      # update object models and refresh this page on any RSVP change
-      self.user.save
-      self.user.reload
-      self.display_event_details(eventname, backpage)
-    end
-
-    # user can view events they've rsvp'd to
-    def show_rsvps
-      self.clear
-      user_events = (self.user.events)
-      if user_events == []
-        # make sure there are events to view
-        puts "You have no events! You should try adding some."
-        sleep 1.5
-        self.display_user_profile
-      else
-        # populate list of RSVP'd events
-        # define code to run for each event if selected
-        # navigation options on bottom of list
-        choices = {}
-        user_events.each do |event|
-          choices[event.name] = "self.display_event_details('#{event.name}', 'self.show_rsvps')"
-        end
-        choices["BACK"] = 'self.display_user_profile'
-        choices["LOGOUT"] = 'self.logout'
-        # selection returns method call as string; execute that call
-        eval(PROMPT.select("Choose an event to see more details", choices, per_page:45))
+  # handle returning users
+  def login_page
+    self.clear
+    input = PROMPT.ask("Welcome back, please enter your username: ")
+    if input.downcase == "back"
+      self.welcome
+    elsif input.downcase == 'exit'
+      self.logout
+    elsif User.find_by(user_name: input)
+      # valid input
+      @user = User.find_by(user_name: input)
+      self.display_user_profile
+    else
+      # if username not found, let them try again or go to new user creation
+      PROMPT.select("That username does not exist") do |menu|
+        menu.choice "Try Again", -> {self.login_page}
+        menu.choice "Create New User", -> {self.create_new_user}
       end
     end
   end
+
+  ########## MAIN PROFILE PAGE ###############################
+
+  # landing page for user profile
+  def display_user_profile
+    self.clear
+    # profile information
+    puts "username: #{self.user.user_name}"
+    puts
+    puts "Name: #{self.user.name}"
+    puts "Location: #{self.user.location}"
+    puts "Interests:"
+    self.list_array(self.user.interests)
+    puts
+    # menu options
+    PROMPT.select ("") do |menu|
+      menu.choice "View Events", -> {self.find_events}
+      menu.choice "View your RSVPs", -> {self.show_rsvps}
+      menu.choice "Edit Profile\n", -> {self.profile_edit}
+      menu.choice "[] LOGOUT", -> {self.logout}
+    end
+  end
+
+  ########## EDITING PROFILE ###############################
+
+  # let user choose various profile information to edit
+  def profile_edit
+    self.clear
+    PROMPT.select("Choose your profile information to edit:") do |menu|
+      menu.choice "Name", -> {self.set_name}
+      menu.choice "Location", -> {self.choose_location}
+      menu.choice "Interests\n", -> {self.add_or_remove_interests}
+      menu.choice "[] BACK", -> {self.display_user_profile}
+      menu.choice "[] LOGOUT", -> {self.logout}
+    end
+    # destination methods would terminate program, so:
+    # save user and reload page once any edits are made
+    self.user.save
+    self.profile_edit
+  end
+
+  # forked choice for user
+  def add_or_remove_interests
+    self.clear
+    PROMPT.select("") do |menu|
+      menu.choice "Add new interests", -> {self.choose_interests}
+      menu.choice "Remove current interests\n", -> {self.remove_interests}
+      menu.choice "[] BACK", -> {self.profile_edit}
+      menu.choice "[] LOGOUT", -> {self.logout}
+    end
+  end
+
+  # user can remove interests they've added to profile
+  def remove_interests
+    self.clear
+    # make sure interests are there to be removed
+    if self.user.interests == []
+      puts "Please add some interests before you try to remove any."
+      sleep 1.5
+      self.add_or_remove_interests
+    else
+      current_interests = self.user.interests.map do |interest|
+        interest.name
+      end
+      # let user select multiple interests to remove
+      selected_interests = PROMPT.multi_select("Please select one or more interests you wish to remove from your profile\n Press 'SPACE' to select and 'ENTER' to finish\n", current_interests, echo:false, per_page:20)
+      # disassociate user from events
+      selected_interests.each do |interest_name|
+        self.user.remove_interest(Interest.find_by(name: interest_name))
+      end
+      # save user and return to option screen
+      self.user.save
+      self.add_or_remove_interests
+    end
+  end
+
+  ########## FIND EVENTS ################
+
+  # show user a list of events in their area matching their interests
+  def find_events
+    self.clear
+    matched_events = self.user.matching_events
+    if self.user.location == nil
+      # make sure location is set
+      puts "Please set your location to see events in your area."
+      sleep 1.5
+      self.display_user_profile
+    elsif self.user.interests == []
+      # make sure interests are set
+      puts "Please choose some interests so we can show you matching events."
+      sleep 1.5
+      self.display_user_profile
+    elsif matched_events == []
+      # just in case there are no matching events
+      puts "No events found in your location matching your interests."
+      sleep 1.5
+      self.display_user_profile
+    else
+      # populate list of matching events
+      # define code to run for each event if selected
+      # tack navigation options onto bottom of list
+      choices = {}
+      matched_events.each do |event|
+        choices[event.name] = "self.display_event_details('#{event.name}', 'self.find_events')"
+      end
+      choices["[] BACK"] = 'self.display_user_profile'
+      choices["[] LOGOUT"] = 'self.logout'
+      # user selection returns as method call in string format; execute that call
+      eval(PROMPT.select("Choose an event to see more details:", choices, per_page:45))
+    end
+  end
+
+  # a more detailed view for a given event
+  def display_event_details(eventname, backpage)
+    self.clear
+    event = Event.find_by(name: eventname)
+    puts "Event Name: #{event.name}"
+    puts "Event Description: #{event.description}"
+    puts "Location: #{event.location}"
+    puts "Date/Time: #{event.event_datetime}"
+    puts "Attendees:"
+    self.list_array(event.users)
+    puts
+
+    # let user add or remove RSVP for event
+    PROMPT.select("") do |menu|
+      if self.user.events.include?(event)
+        menu.choice "Remove your RSVP for this event\n", -> {self.user.remove_event(event)}
+      else
+        menu.choice "RSVP for this event\n", -> {self.user.rsvp_to(event)}
+      end
+      menu.choice "[] BACK", -> {eval(backpage)}
+      menu.choice "[] LOGOUT", -> {self.logout}
+    end
+    # update object models and refresh this page on any RSVP change
+    self.user.save
+    self.user.reload
+    self.display_event_details(eventname, backpage)
+  end
+
+  # user can view events they've rsvp'd to
+  def show_rsvps
+    self.clear
+    user_events = (self.user.events)
+    if user_events == []
+      # make sure there are events to view
+      puts "You have no events! You should try adding some."
+      sleep 1.5
+      self.display_user_profile
+    else
+      # populate list of RSVP'd events
+      # define code to run for each event if selected
+      # navigation options on bottom of list
+      choices = {}
+      user_events.each do |event|
+        choices[event.name] = "self.display_event_details('#{event.name}', 'self.show_rsvps')"
+      end
+      choices["BACK"] = 'self.display_user_profile'
+      choices["LOGOUT"] = 'self.logout'
+      # selection returns method call as string; execute that call
+      eval(PROMPT.select("Choose an event to see more details", choices, per_page:45))
+    end
+  end
+end
